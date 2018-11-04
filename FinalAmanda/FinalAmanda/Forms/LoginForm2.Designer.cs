def169e2136248e6ce42f23de0ae15a3d556b0f6
amanda <amanda@AM-PC>
2018-11-04 15:55:40 -0200

    Espero não estar fazendo nada de errado (de novo)

---
+++ FinalAmanda/FinalAmanda/Forms/LoginForm2.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace FinalAmanda.Forms
+{
+    partial class LoginForm2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlLogFor = new System.Windows.Forms.Panel();
+            this.pbxSend = new System.Windows.Forms.PictureBox();
+            this.pbxLogin = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlLogFor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSend)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlLogFor
+            // 
+            this.pnlLogFor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pnlLogFor.Controls.Add(this.pbxSend);
+            this.pnlLogFor.Controls.Add(this.pbxLogin);
+            this.pnlLogFor.Controls.Add(this.label4);
+            this.pnlLogFor.Controls.Add(this.textBox2);
+            this.pnlLogFor.Controls.Add(this.textBox4);
+            this.pnlLogFor.Controls.Add(this.label3);
+            this.pnlLogFor.Controls.Add(this.textBox1);
+            this.pnlLogFor.Controls.Add(this.label2);
+            this.pnlLogFor.Controls.Add(this.label1);
+            this.pnlLogFor.Location = new System.Drawing.Point(154, 117);
+            this.pnlLogFor.Name = "pnlLogFor";
+            this.pnlLogFor.Size = new System.Drawing.Size(704, 332);
+            this.pnlLogFor.TabIndex = 0;
+            // 
+            // pbxSend
+            // 
+            this.pbxSend.BackColor = System.Drawing.Color.Transparent;
+            this.pbxSend.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbxSend.Image = global::FinalAmanda.Properties.Resources.Send;
+            this.pbxSend.Location = new System.Drawing.Point(229, 235);
+            this.pbxSend.Name = "pbxSend";
+            this.pbxSend.Size = new System.Drawing.Size(100, 29);
+            this.pbxSend.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxSend.TabIndex = 11;
+            this.pbxSend.TabStop = false;
+            // 
+            // pbxLogin
+            // 
+            this.pbxLogin.BackColor = System.Drawing.Color.Transparent;
+            this.pbxLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbxLogin.Image = global::FinalAmanda.Properties.Resources.Enter1;
+            this.pbxLogin.Location = new System.Drawing.Point(591, 235);
+            this.pbxLogin.Name = "pbxLogin";
+            this.pbxLogin.Size = new System.Drawing.Size(100, 29);
+            this.pbxLogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxLogin.TabIndex = 9;
+            this.pbxLogin.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label4.Location = new System.Drawing.Point(375, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Esqueceu sua senha?";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(378, 172);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(313, 33);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(378, 133);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(313, 33);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Corbel", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Aquamarine;
+            this.label3.Location = new System.Drawing.Point(477, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 42);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Entrar";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(14, 155);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(313, 33);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(11, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Digite seu e-mail";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Corbel", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.BlueViolet;
+            this.label1.Location = new System.Drawing.Point(9, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Esqueceu sua senha?";
+            // 
+            // LoginForm2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(1003, 579);
+            this.Controls.Add(this.pnlLogFor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginForm2";
+            this.pnlLogFor.ResumeLayout(false);
+            this.pnlLogFor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSend)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogin)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLogFor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.PictureBox pbxLogin;
+        private System.Windows.Forms.PictureBox pbxSend;
+    }
+}
--- FinalAmanda/FinalAmanda/Program.cs
+++ FinalAmanda/FinalAmanda/Program.cs
@@ -17,7 +17,7 @@ namespace FinalAmanda
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HomeForm());
+            Application.Run(new LoginForm2());
         }
     }
 }
